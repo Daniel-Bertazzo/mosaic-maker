7aa95d6d0f46ba755ba1cf5b48077dbeb230a4d4
Gazo <gabriel.uli@hotmail.com>
2020-06-12 17:34:06 -0300

    main features running

--- source/project.py
+++ source/project.py
@@ -1,9 +1,10 @@
 import numpy as np
 import imageio
 
+
 def downscale(img, n, m):
     """ Takes an image by input and downscale it to the desired resolution """
-    
+
     out_img = np.zeros((n, m, 3))
     xi, yi = img.shape[0:2]
 
@@ -43,16 +44,18 @@ def create_mosaic_division(img, method=1, resolution=(8, 8)):
 
     if method == 1:
         # divides the image in rectagles of the same size
-        n = x / resolution[0]
-        m = y / resolution[1]
-        mosaic = np.zeros((n*m))
-        for i in range(n):
-            for j in range(m):
-                mosaic[i, j] = (i*n, (i+1)*n, j*m, (j+1)*m)
-    
+        n = x // resolution[0]
+        m = y // resolution[1]
+
+        mosaic = np.zeros((resolution[0]*resolution[1], 4), dtype=int)
+
+        for i in range(resolution[0]):
+            for j in range(resolution[1]):
+                mosaic[(i*resolution[1]) + j] = [i*n, (i+1)*n, j*m, (j+1)*m]
+
     # elif method == 2:
     #     mosaic = variado
-    
+
     return mosaic
 
 
@@ -63,9 +66,9 @@ def get_predominant_color(img, method=1):
             method: 1 - mean of all pixels
                     2 - color histogram (with margin?) (200, 200, 200) -- (199, 200, 199)
     """
-    
+
     color = np.zeros((3))
-    
+
     if method == 1:
         # mean of all colors in the image
         color[0] = np.mean(img[:, :, 0])
@@ -74,30 +77,34 @@ def get_predominant_color(img, method=1):
 
     return color
 
+
 def alter_color(img, predominant_color):
     """ alters the color of the image to approximate it to the predominant_color parameter
         
         inputs:
     """
 
-    out_img = img ** 2.2 # transform srgb to linear srgb
+    # out_img = img ** 2.2 # transform srgb to linear srgb
     # out_img = img
     # out_img = np.log(img + 1)
- 
+    out_img = np.array(img, copy=True)
+    # out_img = np.array(img, copy=True) ** 2.2  # transform srgb to linear srgb
+
     # (100, 150, 200) -> (100/(100 + 150 + 200), , ) color example
     out_img[:, :, 0] = out_img[:, :, 0] * (predominant_color[0] / np.sum(predominant_color))
-    out_img[:, :, 1] = out_img[:, :, 1] * (predominant_color[1] / np.sum(predominant_color)) 
-    out_img[:, :, 2] = out_img[:, :, 2] * (predominant_color[2] / np.sum(predominant_color)) 
+    out_img[:, :, 1] = out_img[:, :, 1] * (predominant_color[1] / np.sum(predominant_color))
+    out_img[:, :, 2] = out_img[:, :, 2] * (predominant_color[2] / np.sum(predominant_color))
 
     # out_img += 50
 
-    out_img = out_img ** (1/2.2) # transform back to srgb
+    # out_img = out_img ** (1/2.2) # transform back to srgb
     # out_img = np.exp(out_img)
 
     return np.clip(out_img, 0, 255)
     # return out_img
 
-def mosaic_transform(canvas_img, tile_img, division_method=1,resolution=(8, 8), color_method=1):
+
+def mosaic_transform(canvas_img, tile_img, division_method=1, resolution=(8, 8), color_method=1):
     """ creates a mosaic that represents one image using other images
         
         inputs:
@@ -109,32 +116,47 @@ def mosaic_transform(canvas_img, tile_img, division_method=1,resolution=(8, 8), 
             color_method: 
     """
 
-    mosaic_division = create_mosaic_division(canvas_img, method=division_method, resolution=resolution)
+    mosaic_division = create_mosaic_division(
+        canvas_img, method=division_method, resolution=resolution)
     out_img = np.zeros(canvas_img.shape)
 
+    xi, xf, yi, yf = mosaic_division[0]
+    down_tile = downscale(tile_img, xf - xi, yf - yi)
+
+    print(mosaic_division)
+
+    count = 0
     for subimage in mosaic_division:
         xi, xf, yi, yf = subimage
 
-        
-        predominant_color = get_predominant_color(canvas_img[xi:xf, yi:yf], method=color_method)
-        tile = downscale(tile_img, xf - xi, yf - yi)
-        tile = alter_color(tile, predominant_color)
-        
-        
-    pass
+        count += 1
+        print("subimage: ", count)
+
+        predominant_color = get_predominant_color(
+            canvas_img[xi:xf, yi:yf], method=color_method)
+        # tile = downscale(tile_img, xf - xi, yf - yi)
+        tile = alter_color(down_tile, predominant_color)
+        out_img[xi:xf, yi:yf] = tile
+
+    print("out image: ", canvas_img.shape)
+    print("mosaic division: ", mosaic_division[-1])
+
+    return out_img
+
 
 # reads inputs
-img_name = str(input()).rstrip()
+img_name_canvas = str(input()).rstrip()
+img_name_tile = str(input()).rstrip()
 save = 1
 
 # reads image from '.' directory
-img = imageio.imread(img_name)
+canvas_img = imageio.imread(img_name_canvas)
+tile_img = imageio.imread(img_name_tile)
 
-img = img.astype(np.float)
+canvas_img = canvas_img.astype(np.float)
+tile_img = tile_img.astype(np.float)
 
-cor = np.array([255, 0, 0])
-img_aux = downscale(img, 60, 60)
-out_img = alter_color(img_aux, cor)
+out_img = mosaic_transform(canvas_img, tile_img, resolution=(100, 100))
 
 # verifies if the modified image needs to be saved
 if (save):
